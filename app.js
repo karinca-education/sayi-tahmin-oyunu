6c2655777e434e5a4d6b1da37fb4ab0b7f2bab81
Gülali Çelik <gulalicelikmi@gmail.com>
2021-11-14 21:48:35 +0300

    sayı tahmin oyunu

---
+++ app.js
@@ -0,0 +1,53 @@
+const randomInput = document.getElementById('randomNum');
+const predictedInput = document.getElementById('predictedNum');
+const modalOpacity = document.querySelector('.modal-opacity');
+const modalMessage = document.querySelector('.modal-message');
+const closeButton = document.querySelector('.close');
+const title = document.querySelector('.title');
+const subtitle = document.querySelector('.subtitle');
+
+const handlePrediction = () => {
+  const limitNumber = randomInput.value;
+  var randomNumber = Math.ceil(Math.random() * limitNumber);
+  var predictedNumber = predictedInput.value;
+  if (!randomInput.value) {
+    message(
+      'Bu alan zorunludur',
+      'Limit alanı doldurulması zorunlu bir alandır'
+    );
+    return;
+  }
+  if (!predictedInput.value) {
+    message(
+      'Bu alan zorunludur',
+      'Tahmin alanı doldurulması zorunlu bir alandır'
+    );
+    return;
+  }
+  if (predictedNumber > limitNumber) {
+    message(
+      'HATA!!!1!!1!!1',
+      'Tahmin sayısı ' + parseInt(limitNumber) + ' sayısından büyük olamaz'
+    );
+
+    return;
+  }
+
+  randomNumber == predictedNumber
+    ? message('Tebrikler', 'Doğru tahmin ettiniz')
+    : message('Olmadı ', 'Başaramadık abi, aranılan sayı ' + randomNumber);
+};
+
+closeButton.addEventListener('click', () => {
+  modalMessage.style.visibility = 'hidden';
+  modalOpacity.style.visibility = 'hidden';
+  modalMessage.style.top = '-180px';
+});
+
+const message = (titleMessage, subtitleMessage) => {
+  title.innerText = titleMessage;
+  subtitle.innerText = subtitleMessage;
+  modalMessage.style.visibility = 'visible';
+  modalMessage.style.top = '10px';
+  modalOpacity.style.visibility = 'visible';
+};
